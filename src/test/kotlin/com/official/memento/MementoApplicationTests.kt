0a7e216d1a4556af56d156838608bf0132740451
unanchoi <unanc2@gmail.com>
2024-12-26 22:48:25 +0900

    initial commit

---
+++ src/test/kotlin/com/official/memento/MementoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.official.memento
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MementoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
