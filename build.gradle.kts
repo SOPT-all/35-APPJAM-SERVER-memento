ccad6a7fbac79117791e8daef067a8ddcead5faf
paragon0107 <smg0725@naver.com>
2025-01-08 18:31:52 +0900

    feat: jpa 의존성 및 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 kotlin {
