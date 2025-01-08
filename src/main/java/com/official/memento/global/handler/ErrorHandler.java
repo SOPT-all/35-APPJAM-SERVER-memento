15920cee2b21b1536631a5aff20253d571b23af4
paragon0107 <smg0725@naver.com>
2025-01-08 19:51:57 +0900

    feat: 에러 핸들러 추가

---
+++ src/main/java/com/official/memento/global/handler/ErrorHandler.java
@@ -0,0 +1,18 @@
+package com.official.memento.global.handler;
+
+import com.official.memento.global.dto.ErrorResponse;
+import com.official.memento.global.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exception(Exception exception) {
+        return ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+}
