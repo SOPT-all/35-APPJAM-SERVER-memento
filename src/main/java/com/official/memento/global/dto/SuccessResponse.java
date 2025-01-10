6b1398c1163d1655b70bb22b1cf816ea11d0063a
paragon0107 <smg0725@naver.com>
2025-01-10 17:27:59 +0900

    feat: 공통 DTO 구현

--- src/main/java/com/official/memento/global/dto/SuccessResponse.java
+++ src/main/java/com/official/memento/global/dto/SuccessResponse.java
@@ -1,14 +1,28 @@
 package com.official.memento.global.dto;
 
+import com.official.memento.global.exception.MementoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static com.official.memento.global.exception.ErrorCode.NULL_DATA_ERROR;
+
 public record SuccessResponse<T>(
         String message,
         T data
 ) {
-    public static <T> SuccessResponse<T> success(final String message, final T data) {
-        return new SuccessResponse<T>( message, data);
+    public static <T> ResponseEntity<SuccessResponse<?>> of(final HttpStatus status, final String message, final T data) {
+        if (status == null || message == null || data == null) {
+            throw new MementoException(NULL_DATA_ERROR);
+        }
+        return ResponseEntity.status(status.value())
+                .body(new SuccessResponse<T>(message, data));
     }
 
-    public static SuccessResponse<?> success(final String message) {
-        return new SuccessResponse<>( message, "");
+    public static ResponseEntity<SuccessResponse<?>> of(final HttpStatus status, final String message) {
+        if (status == null || message == null) {
+            throw new MementoException(NULL_DATA_ERROR);
+        }
+        return ResponseEntity.status(status.value())
+                .body(new SuccessResponse<>(message, ""));
     }
 }
