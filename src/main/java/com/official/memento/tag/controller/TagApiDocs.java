81966c871dbccfcfb08e72fa7f749e9f2ff23795
unanchoi <unanc2@gmail.com>
2025-01-14 21:48:41 +0900

    update: Tag 생성 API Swagger 업데이트

--- src/main/java/com/official/memento/tag/controller/TagApiDocs.java
+++ src/main/java/com/official/memento/tag/controller/TagApiDocs.java
@@ -1,4 +1,27 @@
 package com.official.memento.tag.controller;
 
+import com.official.memento.global.annotation.Authorization;
+import com.official.memento.global.annotation.AuthorizationUser;
+import com.official.memento.global.dto.SuccessResponse;
+import com.official.memento.tag.controller.dto.TagCreateRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "[Tag API] 태그 관련 API")
 public interface TagApiDocs {
+
+    @Operation(description = "태그 생성 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    ResponseEntity<SuccessResponse<?>> createTag(
+            @Authorization final AuthorizationUser authorizationUser,
+            @RequestBody final TagCreateRequest request
+    );
 }
