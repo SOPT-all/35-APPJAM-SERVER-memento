ae39e02716d82aba28d6a9ccec94709910ccf39b
paragon0107 <smg0725@naver.com>
2025-01-15 23:40:53 +0900

    feat: 태그 연결

--- src/main/java/com/official/memento/schedule/service/ScheduleService.java
+++ src/main/java/com/official/memento/schedule/service/ScheduleService.java
@@ -2,7 +2,10 @@ package com.official.memento.schedule.service;
 
 import com.official.memento.schedule.domain.Schedule;
 import com.official.memento.schedule.domain.ScheduleRepository;
+import com.official.memento.schedule.domain.ScheduleTag;
+import com.official.memento.schedule.domain.ScheduleTagRepository;
 import com.official.memento.schedule.service.command.ScheduleCreateCommand;
+import com.official.memento.tag.domain.TagRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,16 +14,37 @@ import static com.official.memento.schedule.domain.enums.ScheduleType.NORMAL;
 @Service
 public class ScheduleService implements ScheduleCreateUseCase {
 
+    private final ScheduleTagRepository scheduleTagRepository;
     private final ScheduleRepository scheduleRepository;
+    private final TagRepository tagRepository;
 
-    public ScheduleService(ScheduleRepository scheduleRepository) {
+    public ScheduleService(
+            final ScheduleRepository scheduleRepository,
+            final ScheduleTagRepository scheduleTagRepository,
+            final TagRepository tagRepository
+    ) {
         this.scheduleRepository = scheduleRepository;
+        this.scheduleTagRepository = scheduleTagRepository;
+        this.tagRepository = tagRepository;
     }
 
     @Override
     @Transactional
     public Schedule create(final ScheduleCreateCommand command) {
-        Schedule schedule = Schedule.of(
+        Schedule schedule = createSchedule(command);
+        connectTag(command, schedule);
+        //순서관련 로직 추가
+        return schedule;
+    }
+
+    private void connectTag(final ScheduleCreateCommand command, final Schedule schedule) {
+        tagRepository.findById(command.tagId());
+        ScheduleTag scheduleTag = ScheduleTag.of(command.tagId(), schedule.getId());
+        scheduleTagRepository.save(scheduleTag);
+    }
+
+    private Schedule createSchedule(final ScheduleCreateCommand command) {
+        return scheduleRepository.save(Schedule.of(
                 command.memberId(),
                 command.description(),
                 command.startDate(),
@@ -30,10 +54,6 @@ public class ScheduleService implements ScheduleCreateUseCase {
                 command.repeatExpiredDate(),
                 NORMAL,
                 "일단 유니크 스케줄 그룹 아이디"
-        );
-        scheduleRepository.save(schedule);
-        //순서관련 로직 추가
-        return schedule;
+        ));
     }
-
 }
