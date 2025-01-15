a8c744554ed84bd1a0453565a4b61f1d621c2a51
paragon0107 <smg0725@naver.com>
2025-01-15 20:49:18 +0900

    feat: ScheduleEntity 추가

--- src/main/java/com/official/memento/schedule/infrastructure/persistence/ScheduleEntity.java
+++ src/main/java/com/official/memento/schedule/infrastructure/persistence/ScheduleEntity.java
@@ -1,7 +1,9 @@
 package com.official.memento.schedule.infrastructure.persistence;
 
 import com.official.memento.global.entity.BaseTimeEntity;
+import com.official.memento.schedule.domain.Schedule;
 import com.official.memento.schedule.domain.enums.RepeatOption;
+import com.official.memento.schedule.domain.enums.ScheduleType;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
@@ -10,15 +12,52 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "schedule")
 public class ScheduleEntity extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long memberId;
+    private long memberId;
     private String description;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private boolean isAllDay;
     private RepeatOption repeatOption;
     private LocalDate repeatExpiredDate;
-    private String type;
+    private ScheduleType type;
+
+    public ScheduleEntity() {
+    }
+
+    public ScheduleEntity(
+            final long memberId,
+            final String description,
+            final LocalDateTime startDate,
+            final LocalDateTime endDate,
+            final boolean isAllDay,
+            final RepeatOption repeatOption,
+            final LocalDate repeatExpiredDate,
+            final ScheduleType type
+    ) {
+        this.memberId = memberId;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isAllDay = isAllDay;
+        this.repeatOption = repeatOption;
+        this.repeatExpiredDate = repeatExpiredDate;
+        this.type = type;
+    }
+
+    public static ScheduleEntity of(final Schedule schedule) {
+        return new ScheduleEntity(
+                schedule.getMemberId(),
+                schedule.getDescription(),
+                schedule.getStartDate(),
+                schedule.getEndDate(),
+                schedule.isAllDay(),
+                schedule.getRepeatOption(),
+                schedule.getRepeatExpiredDate(),
+                schedule.getType()
+        );
+    }
 }
