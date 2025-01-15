964d552d2c95bff7e0d061445128f8e65bb62eae
paragon0107 <smg0725@naver.com>
2025-01-15 20:56:13 +0900

    feat: 스케줄 도메인 엔티티 구현

---
+++ src/main/java/com/official/memento/schedule/domain/Schedule.java
@@ -0,0 +1,156 @@
+package com.official.memento.schedule.domain;
+
+import com.official.memento.schedule.domain.enums.RepeatOption;
+import com.official.memento.schedule.domain.enums.ScheduleType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Schedule {
+    private Long id;
+    private long memberId;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private boolean isAllDay;
+    private RepeatOption repeatOption;
+    private LocalDate repeatExpiredDate;
+    private ScheduleType type;
+    private String scheduleGroupId;
+
+    private Schedule(
+            final Long id,
+            final long memberId,
+            final String description,
+            final LocalDateTime startDate,
+            final LocalDateTime endDate,
+            final boolean isAllDay,
+            final RepeatOption repeatOption,
+            final LocalDate repeatExpiredDate,
+            final ScheduleType type,
+            final String scheduleGroupId
+    ) {
+        this.id = id;
+        this.memberId = memberId;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isAllDay = isAllDay;
+        this.repeatOption = repeatOption;
+        this.repeatExpiredDate = repeatExpiredDate;
+        this.type = type;
+        this.scheduleGroupId = scheduleGroupId;
+    }
+
+    private Schedule(
+            final long memberId,
+            final String description,
+            final LocalDateTime startDate,
+            final LocalDateTime endDate,
+            final boolean isAllDay,
+            final RepeatOption repeatOption,
+            final LocalDate repeatExpiredDate,
+            final ScheduleType type,
+            final String scheduleGroupId
+    ) {
+        this.memberId = memberId;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isAllDay = isAllDay;
+        this.repeatOption = repeatOption;
+        this.repeatExpiredDate = repeatExpiredDate;
+        this.type = type;
+        this.scheduleGroupId = scheduleGroupId;
+    }
+
+    public static Schedule withId(
+            final Long id,
+            final long memberId,
+            final String description,
+            final LocalDateTime startDate,
+            final LocalDateTime endDate,
+            final boolean isAllDay,
+            final RepeatOption repeatOption,
+            final LocalDate repeatExpiredDate,
+            final ScheduleType type,
+            final String scheduleGroupId
+    ) {
+        return new Schedule(
+                id,
+                memberId,
+                description,
+                startDate,
+                endDate,
+                isAllDay,
+                repeatOption,
+                repeatExpiredDate,
+                type,
+                scheduleGroupId
+        );
+    }
+
+    public static Schedule of(
+            final long memberId,
+            final String description,
+            final LocalDateTime startDate,
+            final LocalDateTime endDate,
+            final boolean isAllDay,
+            final RepeatOption repeatOption,
+            final LocalDate repeatExpiredDate,
+            final ScheduleType type,
+            final String scheduleGroupId
+    ) {
+        return new Schedule(
+                memberId,
+                description,
+                startDate,
+                endDate,
+                isAllDay,
+                repeatOption,
+                repeatExpiredDate,
+                type,
+                scheduleGroupId
+        );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public long getMemberId() {
+        return memberId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public boolean isAllDay() {
+        return isAllDay;
+    }
+
+    public RepeatOption getRepeatOption() {
+        return repeatOption;
+    }
+
+    public LocalDate getRepeatExpiredDate() {
+        return repeatExpiredDate;
+    }
+
+    public ScheduleType getType() {
+        return type;
+    }
+
+    public String getScheduleGroupId() {
+        return scheduleGroupId;
+    }
+}
