a85fc05dfea8c3428a0acf200ea372235c6d8793
paragon0107 <smg0725@naver.com>
2025-01-17 03:49:32 +0900

    feat: 스케줄 컨트롤러 구현

--- src/main/java/com/official/memento/schedule/conntroller/ScheduleApiController.java
+++ src/main/java/com/official/memento/schedule/conntroller/ScheduleApiController.java
@@ -4,9 +4,11 @@ import com.official.memento.global.annotation.Authorization;
 import com.official.memento.global.annotation.AuthorizationUser;
 import com.official.memento.global.dto.SuccessResponse;
 import com.official.memento.schedule.conntroller.dto.request.ScheduleCreateRequest;
-import com.official.memento.schedule.conntroller.dto.response.ScheduleCreateResponse;
+import com.official.memento.schedule.conntroller.dto.request.RepeatScheduleCreateRequest;
+import com.official.memento.schedule.service.RepeatScheduleCreateUseCase;
 import com.official.memento.schedule.service.ScheduleCreateUseCase;
 import com.official.memento.schedule.service.command.ScheduleCreateCommand;
+import com.official.memento.schedule.service.command.RepeatScheduleCreateCommand;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,13 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScheduleApiController {
 
     private final ScheduleCreateUseCase scheduleCreateUseCase;
+    private final RepeatScheduleCreateUseCase repeatScheduleCreateUseCase;
 
-    public ScheduleApiController(ScheduleCreateUseCase scheduleCreateUseCase) {
+    public ScheduleApiController(
+            final ScheduleCreateUseCase scheduleCreateUseCase,
+            final RepeatScheduleCreateUseCase repeatScheduleCreateUseCase
+    ) {
         this.scheduleCreateUseCase = scheduleCreateUseCase;
+        this.repeatScheduleCreateUseCase = repeatScheduleCreateUseCase;
     }
 
     @PostMapping
-    ResponseEntity<SuccessResponse<ScheduleCreateResponse>> createSchedule(
+    ResponseEntity<SuccessResponse<?>> createSchedule(
             @Authorization final AuthorizationUser authorizationUser,
             @RequestBody final ScheduleCreateRequest scheduleCreateRequest
     ) {
@@ -36,15 +43,35 @@ public class ScheduleApiController {
                         scheduleCreateRequest.startDate(),
                         scheduleCreateRequest.endDate(),
                         scheduleCreateRequest.isAllDay(),
-                        scheduleCreateRequest.repeatOption(),
-                        scheduleCreateRequest.repeatExpiredDate(),
                         scheduleCreateRequest.tagId()
                 )
         );
         return SuccessResponse.of(
                 HttpStatus.CREATED,
-                "스케줄 생성 성공",
-                ScheduleCreateResponse.of()
+                "단일 스케줄 생성 성공"
+        );
+    }
+
+    @PostMapping("/repetition")
+    ResponseEntity<SuccessResponse<?>> createScheduleMultiple(
+            @Authorization final AuthorizationUser authorizationUser,
+            @RequestBody final RepeatScheduleCreateRequest repeatScheduleCreateRequest
+    ) {
+        repeatScheduleCreateUseCase.createRepeat(
+                RepeatScheduleCreateCommand.of(
+                        authorizationUser.memberId(),
+                        repeatScheduleCreateRequest.description(),
+                        repeatScheduleCreateRequest.startDate(),
+                        repeatScheduleCreateRequest.endDate(),
+                        repeatScheduleCreateRequest.isAllDay(),
+                        repeatScheduleCreateRequest.repeatOption(),
+                        repeatScheduleCreateRequest.repeatExpiredDate(),
+                        repeatScheduleCreateRequest.tagId()
+                )
+        );
+        return SuccessResponse.of(
+                HttpStatus.CREATED,
+                "반복 스케줄 생성 성공"
         );
     }
 }
