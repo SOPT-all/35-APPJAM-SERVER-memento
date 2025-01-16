754a0b74ec1716a1ac2d8bd89be2eb16c33620d7
unanchoi <unanc2@gmail.com>
2025-01-16 15:21:37 +0900

    fix: 사분면 별 중심좌표 수정

--- src/main/java/com/official/memento/todo/domain/enums/EisenhowerMatrixQuadrant.java
+++ src/main/java/com/official/memento/todo/domain/enums/EisenhowerMatrixQuadrant.java
@@ -1,9 +1,9 @@
 package com.official.memento.todo.domain.enums;
 
 public enum EisenhowerMatrixQuadrant {
-    QUADRANT_1(PriorityType.HIGH, .25, 0.25),
-    QUADRANT_2(PriorityType.IMMEDIATE, 0.25, 0.25),
-    QUADRANT_3(PriorityType.MEDIUM,.25, 0.25),
+    QUADRANT_1(PriorityType.HIGH, 0.25, 0.75),
+    QUADRANT_2(PriorityType.IMMEDIATE, 0.75, 0.75),
+    QUADRANT_3(PriorityType.MEDIUM,0.75, 0.25),
     QUADRANT_4(PriorityType.LOW, 0.25, 0.25);
 
     private final PriorityType priorityType;
