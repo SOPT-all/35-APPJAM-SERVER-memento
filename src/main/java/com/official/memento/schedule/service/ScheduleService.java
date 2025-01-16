a2c3f2f04e037a28551a41ecb623e760758974d8
paragon0107 <smg0725@naver.com>
2025-01-17 03:49:39 +0900

    feat: 스케줄 서비스 구현

--- src/main/java/com/official/memento/schedule/service/ScheduleService.java
+++ src/main/java/com/official/memento/schedule/service/ScheduleService.java
@@ -1,18 +1,26 @@
 package com.official.memento.schedule.service;
 
+import com.official.memento.global.entity.enums.RepeatOption;
 import com.official.memento.schedule.domain.Schedule;
 import com.official.memento.schedule.domain.ScheduleRepository;
 import com.official.memento.schedule.domain.ScheduleTag;
 import com.official.memento.schedule.domain.ScheduleTagRepository;
+import com.official.memento.schedule.service.command.RepeatScheduleCreateCommand;
 import com.official.memento.schedule.service.command.ScheduleCreateCommand;
 import com.official.memento.tag.domain.TagRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import static com.official.memento.schedule.domain.enums.ScheduleType.NORMAL;
 
 @Service
-public class ScheduleService implements ScheduleCreateUseCase {
+public class ScheduleService implements ScheduleCreateUseCase, RepeatScheduleCreateUseCase {
 
     private final ScheduleTagRepository scheduleTagRepository;
     private final ScheduleRepository scheduleRepository;
@@ -30,30 +38,78 @@ public class ScheduleService implements ScheduleCreateUseCase {
 
     @Override
     @Transactional
-    public Schedule create(final ScheduleCreateCommand command) {
-        Schedule schedule = createSchedule(command);
-        connectTag(command, schedule);
+    public void create(final ScheduleCreateCommand command) {
+        String scheduleGroupId = UUID.randomUUID().toString();
+        Schedule schedule = createSchedule(command, scheduleGroupId);
+        connectTag(command.tagId(), schedule);
         //순서관련 로직 추가
-        return schedule;
     }
 
-    private void connectTag(final ScheduleCreateCommand command, final Schedule schedule) {
-        tagRepository.findById(command.tagId());
-        ScheduleTag scheduleTag = ScheduleTag.of(command.tagId(), schedule.getId());
-        scheduleTagRepository.save(scheduleTag);
+    @Override
+    @Transactional
+    public void createRepeat(final RepeatScheduleCreateCommand command) {
+        String scheduleGroupId = UUID.randomUUID().toString();
+        List<Schedule> schedules = null;
+        switch (command.repeatOption()) {
+            case DAILY -> schedules = createRepeatSchedules(command, scheduleGroupId, 1);
+            case WEEKLY -> schedules = createRepeatSchedules(command, scheduleGroupId, 7);
+            case YEARLY -> schedules = createRepeatSchedules(command, scheduleGroupId, 365);
+            default ->
+                    throw new IllegalArgumentException("Unsupported repeat option: " + command.repeatOption());//커스텀 익셉션으로 교체 예정
+        }
+        connectTags(command.tagId(), schedules);
+        //순서관련 로직 추가
     }
 
-    private Schedule createSchedule(final ScheduleCreateCommand command) {
+    private Schedule createSchedule(final ScheduleCreateCommand command, final String scheduleGroupId) {
         return scheduleRepository.save(Schedule.of(
                 command.memberId(),
                 command.description(),
                 command.startDate(),
                 command.endDate(),
                 command.isAllDay(),
-                command.repeatOption(),
-                command.repeatExpiredDate(),
+                RepeatOption.NONE,
+                null,
                 NORMAL,
-                "일단 유니크 스케줄 그룹 아이디"
+                scheduleGroupId
         ));
+    }
+
+    private List<Schedule> createRepeatSchedules(
+            final RepeatScheduleCreateCommand command,
+            final String scheduleGroupId,
+            final int afterStartDay
+    ) {
+        List<Schedule> schedules = new ArrayList<>();
+        LocalDateTime currentStartDate = command.startDate();
+        LocalDateTime currentEndDate = command.endDate();
+        LocalDate repeatExpiredDate = command.repeatExpiredDate();
+        while (!currentEndDate.toLocalDate().isAfter(repeatExpiredDate)) {
+            Schedule schedule = scheduleRepository.save(Schedule.of(
+                    command.memberId(),
+                    command.description(),
+                    currentStartDate,
+                    currentEndDate,
+                    command.isAllDay(),
+                    RepeatOption.DAILY,
+                    repeatExpiredDate,
+                    NORMAL,
+                    scheduleGroupId
+            ));
+            schedules.add(schedule);
+            currentStartDate = currentStartDate.plusDays(afterStartDay);
+            currentEndDate = currentEndDate.plusDays(afterStartDay);
+        }
+        return schedules;
+    }
+
+    private void connectTags(final Long tagId, final List<Schedule> schedules) {
+        schedules.forEach(schedule -> connectTag(tagId, schedule));
+    }
+
+    private void connectTag(final Long tagId, final Schedule schedule) {
+        tagRepository.findById(tagId);
+        ScheduleTag scheduleTag = ScheduleTag.of(tagId, schedule.getId());
+        scheduleTagRepository.save(scheduleTag);
     }
 }
